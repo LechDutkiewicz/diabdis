002ccde93489e7ab586db7412e8ce4427b90d49b
LechDutkiewicz <lechdutkiewicz@gmail.com>
2015-02-02 10:43:56 +0100

    changed category structure

--- templates/blog-cat-navi.php
+++ templates/blog-cat-navi.php
@@ -3,8 +3,9 @@
 if ( !defined( 'ABSPATH' ) )
 	exit( 'No direct script access allowed' ); // Exit if accessed directly
 // setup variables
-$blogCats = get_field('blog_root_cat', 'options');
-$categories = get_children_categories($blogCats->term_id);
+/*$blogCats = get_field('blog_root_cat', 'options');
+$categories = get_children_categories($blogCats->term_id);*/
+$categories = get_categories();
 
 ?>
 
